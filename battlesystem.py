2e76fd620be18146162ef5dfaa9bf7398638c675
EthanHunt404 <etbuttazzi@hotmail.com>
2024-04-03 03:29:18 -0300

    On branch developer Your branch is up to date with 'origin/developer'. Changes to be committed: modified:   battlesystem.py

--- battlesystem.py
+++ battlesystem.py
@@ -4,17 +4,18 @@ def Battle(state : bool):
     
     InBattle : bool = state
     
-    capybara = {HP : 30, ATK : 3, DEF : 0}
-    capybaraBonus = {HPb : 0, ATKb : 0, DEFb : 0}
+    capybara = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
+    capybaraBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
     
-    player = {HP : 30, ATK : 3, DEF : 0}
-    playerBonus = {HPb : 0, ATKb : 0, DEFb : 0}
+    player = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
+    playerBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
     
     while InBattle:
-        capybara[HP] += capybaraBonus[HPb]
-        capybara[ATK] += capybaraBonus[ATKb]
-        capybara[DEF] += capybaraBonus[DEFb]
+        capybara['HP'] += capybaraBonus['HPb']
+        capybara['ATK'] += capybaraBonus['ATKb']
+        capybara['DEF'] += capybaraBonus['DEFb']
         
-        player[HP] += capybaraBonus[HPb]
-        player[ATK] += capybaraBonus[ATKb]
-        player[DEF] += capybaraBonus[DEFb]
+        player['HP'] += capybaraBonus['HPb']
+        player['ATK'] += capybaraBonus['ATKb']
+        player['DEF'] += capybaraBonus['DEFb']
+        
