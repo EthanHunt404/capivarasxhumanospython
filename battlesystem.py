882034c7f09118ba2ac55226f30253c1ae445210
EthanHunt404 <etbuttazzi@hotmail.com>
2024-04-02 23:26:13 -0300

    spelling fix

--- battlesystem.py
+++ battlesystem.py
@@ -4,17 +4,17 @@ def Battle(state : bool):
     
     InBattle : bool = state
     
-    capibara = {HP : 30, ATK : 3, DEF : 0}
-    capibaraBonus = {HPb : 0, ATKb : 0, DEFb : 0}
+    capybara = {HP : 30, ATK : 3, DEF : 0}
+    capybaraBonus = {HPb : 0, ATKb : 0, DEFb : 0}
     
     player = {HP : 30, ATK : 3, DEF : 0}
     playerBonus = {HPb : 0, ATKb : 0, DEFb : 0}
     
     while InBattle:
-        capibara[HP] += capibaraBonus[HPb]
-        capibara[ATK] += capibaraBonus[ATKb]
-        capibara[DEF] += capibaraBonus[DEFb]
+        capybara[HP] += capybaraBonus[HPb]
+        capybara[ATK] += capybaraBonus[ATKb]
+        capybara[DEF] += capybaraBonus[DEFb]
         
-        player[HP] += capibaraBonus[HPb]
-        player[ATK] += capibaraBonus[ATKb]
-        player[DEF] += capibaraBonus[DEFb]
+        player[HP] += capybaraBonus[HPb]
+        player[ATK] += capybaraBonus[ATKb]
+        player[DEF] += capybaraBonus[DEFb]
