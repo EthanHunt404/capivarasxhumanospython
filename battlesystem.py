acd49b4e4974d410deb6f52d894cc735e9bd1077
EthanHunt404 <etbuttazzi@hotmail.com>
2024-04-03 19:57:37 -0300

    progress

--- battlesystem.py
+++ battlesystem.py
@@ -1,14 +1,28 @@
 import random
 
+capybara = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
+capybaraBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
+capybaraTitle = {"" : ""}
+
+player = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
+playerBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
+
+capybaraImg = file.read('capybara.txt')
+BattleScreen : str = f"""
+--------------------------------------------------------------------------
+{capybara['HP']}/Health {capybara['ATK']}/Attack {capybara['DEF']}/Defense
+--------------------------------------------------------------------------
+{capybaraImg}
+----------------------------------------------------------------------
+
+----------------------------------------------------------------------
+"""
+
 def Battle(state : bool):
     
+    print("A battle has commenced")
+        
     InBattle : bool = state
-    
-    capybara = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
-    capybaraBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
-    
-    player = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
-    playerBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
     
     while InBattle:
         capybara['HP'] += capybaraBonus['HPb']
@@ -18,4 +32,5 @@ def Battle(state : bool):
         player['HP'] += capybaraBonus['HPb']
         player['ATK'] += capybaraBonus['ATKb']
         player['DEF'] += capybaraBonus['DEFb']
+        
         
