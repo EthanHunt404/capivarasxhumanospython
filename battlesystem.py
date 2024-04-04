beef25e3e74dc5d19ca7378543a9444e15a4c30a
EthanHunt404 <etbuttazzi@hotmail.com>
2024-04-03 21:20:38 -0300

    Progress

--- battlesystem.py
+++ battlesystem.py
@@ -3,19 +3,35 @@ import AsciiImgs
 
 capybara = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
 capybaraBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
-capybaraTitle = {"" : ""}
+capybaraTitle = ''
 
 player = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
 playerBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
 
-BattleScreen = f"""
---------------------------------------------------------------------------
-{capybara['HP']}/Health {capybara['ATK']}/Attack {capybara['DEF']}/Defense
---------------------------------------------------------------------------
-{AsciiImgs.capybaraImg}
-----------------------------------------------------------------------
+def SelTitle(subject : str):
+    adjectiveList = ('Fat', 'Unyielding', 'Smiling', 'Classy', 'Discreet', 'Amused', 'Stern', 'Exalted', 'Air Head', 
+                      'Billowy', 'Fluffy', 'Willful', 'Jealous', 'Cautious', 'Nasty', 'Mean', 'Hairless', 'Baby', 
+                      'Poor', 'Bright', 'Shaman', 'Electric', 'Worthless', 'Clueless', 
+                      'Rat', 'Saint', 'Thin', 'Old', 'Strong', 'Envious', 'Animated', 'Decomposing', 
+                      'Quirky', 'Nutritious', 'Shallow', 'Obscene', 'Ablaze', 'Valuable', 'Elated')
+    lottery = random.randint(0, 39)
+    
+    subject = adjectiveList[lottery] + ' Capybara'
+    
+    return subject
+    
+    
 
-----------------------------------------------------------------------
-"""
+def StartBattle():
+    BattleScreen : str = f"""
+    --------------------------------------------------------------------------
+                            
+    {capybara['HP']}/Health {capybara['ATK']}/Attack {capybara['DEF']}/Defense
+    --------------------------------------------------------------------------
+    {AsciiImgs.capybaraImg}
+    ----------------------------------------------------------------------
 
-print(BattleScreen)
+    ----------------------------------------------------------------------
+    """
+
+print(SelTitle(capybara))
