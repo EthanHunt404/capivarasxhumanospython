52e89ee3a5a869f1bcba6f8619e90b26c5d2f43a
EthanHunt404 <etbuttazzi@hotmail.com>
2024-04-10 18:23:13 -0300

    progress

--- battlesystem.py
+++ battlesystem.py
@@ -1,14 +1,5 @@
-import sys
 import random
 import AsciiImgs
-
-
-capybara = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
-capybaraBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
-capybaraTitle = ''
-
-player = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
-playerBonus = {'HPb' : 0, 'ATKb' : 0, 'DEFb' : 0}
 
 def SelTitle():
     adjectiveList = ('Fat', 'Unyielding', 'Smiling', 'Classy', 'Discreet', 'Amused', 'Stern', 'Exalted', 'Air Head', 
@@ -18,49 +9,72 @@ def SelTitle():
                       'Quirky', 'Nutritious', 'Shallow', 'Obscene', 'Ablaze', 'Valuable', 'Elated')
     lottery = random.randint(0, 39)
     
-    subject = adjectiveList[lottery] + ' Capybara'
+    subject =  f'{adjectiveList[lottery]} Capybara'
     
     return subject
-    
-    
 
-def StartBattle(GameOn : bool):
+def BattleScreen(capybara : dict, capybaramax : dict, player : dict, playermax : dict, capybaratitle : str):
     
-    GameOn = True
-    
-    while GameOn:
-        Enemy = SelTitle()
-        
-        
-        BattleScreen = f"""
+    GUI = f"""
 -------------------------------------------------------------------
-                        {Enemy}
-    {capybara['HP']}/Health             {capybara['ATK']}/Attack                {capybara['DEF']}/Defense
+                        {capybaratitle}
+    {capybara['HP']}/{capybaramax['MAX_HP']} Health             {capybara['ATK']}/{capybaramax['MAX_ATK']} Attack                {capybara['DEF']}/{capybaramax['MAX_DEF']} Defense
 -------------------------------------------------------------------
 {AsciiImgs.capybaraImg}
 -------------------------------------------------------------------
                             Player
-    {player['HP']}/Health               {player['ATK']}/Attack                  {player['DEF']}/Defense
--------------------------------------------------------------------"""
+    {player['HP']}/{playermax['MAX_HP']} Health               {player['ATK']}/{playermax['MAX_ATK']} Attack                  {player['DEF']}/{playermax['MAX_DEF']} Defense
+----------------------------------------------1---------------------"""
+    return print(GUI)
 
-        AsciiImgs.printUTF8(BattleScreen)
+def Flee():
+    
+    roll = random.randint(1, 10)
+    
+    if roll == 10:
+        return True
+    else:
+        return False
+
+def StartBattle(GameOn : bool):
+    
+    capybaratitle = SelTitle()
+    
+    capybaramax = {'MAX_HP' : 30, 'MAX_ATK' : 30, 'MAX_DEF' : 30}
+    capybara = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
+
+    playermax = {'MAX_HP' : 30, 'MAX_ATK' : 30, 'MAX_DEF' : 30}
+    player = {'HP' : 30, 'ATK' : 3, 'DEF' : 0}
+    playerDefending = False
+    
+    while GameOn:
+
+        BattleScreen(capybara, capybaramax, player, playermax, capybaratitle)
         
         while True:
-            Selection = input("""
-A foe has appeared, what do you do
-> 1: Attack (a simple attack)
-> 2: Defend (diminish damage equal to your Defense attribute)
-> 3: Flee (1/10 change o skipping this fight, but you dont level up)\n""")
+            Selection = input("A foe has appeared, what do you do\n> 1: Attack (a simple attack)\n> 2: Defend (diminish damage equal to your Defense attribute)\n> 3: Flee (1/10 change o skipping this fight, but you dont level up)\n")
             
             Selection = Selection.upper()
             
             if Selection in ("1", "ATTACK", "2", "DEFEND", "3", "FLEE"):
                 break
             elif Selection in ("CLOSE","BREAK", "EXIT"):
-                print("Goodbye.\n")
+                print("\nGoodbye.\n")
                 GameOn = False
                 break
             else:
-                print(f"[{Selection}] is not a valid option choose a valid option")
+                print(f"\n[{Selection}] is not a valid option choose a valid option\n")
     
+        if Selection in ("1", "ATTACK"):
+            capybara['HP'] = capybara['HP'] - player['ATK']
+        elif Selection in ("2", "DEFEND"):
+            playerDefending = True
+        elif Selection in ("3", "FLEE"):
+            roll = Flee()
+            if roll == True:
+                GameOn = False
+                break
+            else:
+                continue
+
 StartBattle(True)
